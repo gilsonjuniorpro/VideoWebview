082cccddfec8e3cc6d57b5a545ec57ea5b91893a
gilsonjuniorpro <gilsonjuniorpro@gmail.com>
2021-03-04 18:30:38 -0500

    creating element

--- app/src/main/java/videowebview/ca/MainActivity.kt
+++ app/src/main/java/videowebview/ca/MainActivity.kt
@@ -2,19 +2,23 @@ package videowebview.ca
 
 import android.content.Context
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
+import android.provider.MediaStore
 import android.util.Log
 import android.webkit.*
 import android.widget.Toast
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
-import androidx.webkit.WebViewAssetLoader
 import androidx.webkit.WebViewClientCompat
 import videowebview.ca.databinding.ActivityMainBinding
-import java.io.File
-import java.net.URLConnection
+import java.io.*
+
 
 class MainActivity : AppCompatActivity() {
+
     lateinit var it: Intent
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityMainBinding.inflate(layoutInflater)
@@ -24,17 +28,33 @@ class MainActivity : AppCompatActivity() {
 
         binding.webview.webViewClient = MyWebViewClient(this)
         binding.webview.settings.apply {
-            useWideViewPort = true
+            //useWideViewPort = true
             loadWithOverviewMode = true
             javaScriptEnabled = true
-            allowFileAccessFromFileURLs = true
+            //allowFileAccessFromFileURLs = true
+
+            //when disabled the video don't plays
             allowUniversalAccessFromFileURLs = true
             allowFileAccess = true
             allowContentAccess = true
+
             WebSettings.PluginState.ON
         }
 
-        binding.webview.loadUrl("file:///android_asset/index.html")
+
+
+        val uuid = "SrQzkLcJqjr1efvssLM9Rr"
+
+        var fileString = readAsset(baseContext, "index.html", uuid)
+        saveTextFile(fileString)
+
+        //"file:///android_asset/page.html"
+        //var uri = content://media/external/images/media/174518
+        //file:///data/user/0/com.raptorswatermark.ca/cache/cropped3845631742083647187.jpg
+
+        binding.webview.loadUrl("file:///data/user/0/videowebview.ca/files/index.html")
+        //binding.webview.loadUrl("file:///android_asset/index.html")
+
 
         binding.webview.webChromeClient = object : WebChromeClient() {
             override fun onConsoleMessage(message: ConsoleMessage): Boolean {
@@ -46,39 +66,46 @@ class MainActivity : AppCompatActivity() {
         binding.webview.addJavascriptInterface(WebAppInterface(this), "Android")
     }
 
+    private fun saveTextFile(file: String) {
+        try {
+            val fileOutputStream: FileOutputStream = openFileOutput("index.html", Context.MODE_PRIVATE)
+            val outputWriter = OutputStreamWriter(fileOutputStream)
+            outputWriter.write(file)
+            outputWriter.close()
+        }
+        catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
+
+    fun readAsset(context: Context, fileName: String, uuid: String): String =
+            context.assets
+                    .open(fileName)
+                    .bufferedReader()
+                    .use(BufferedReader::readText).replace("<uuid>", uuid)
+
     class MyWebViewClient(private val context: Context) : WebViewClientCompat() {
         override fun onPageFinished(view: WebView, url: String) {
-            val url = "https://play.vidyard.com/SrQzkLcJqjr1efvssLM9Rr.jpg"
-            //val url = "https://gamingtrend.com/wp-content/uploads/2020/07/youtube-thumb.jpg"
+            /*val url = "https://play.vidyard.com/SrQzkLcJqjr1efvssLM9Rr.jpg"
             val token = "SrQzkLcJqjr1efvssLM9Rr"
 
             if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.KITKAT) {
                 view.evaluateJavascript("loadMsg('$url', '$token')", null)
             } else {
                 view.loadUrl("javascript:loadMsg('$url', '$token')")
-            }
+            }*/
         }
     }
 
     class WebAppInterface(private val mContext: Context) {
-        /** Show a toast from the web page  */
         @JavascriptInterface
-        fun showToast(toast: String) {
-            Toast.makeText(mContext, toast, Toast.LENGTH_SHORT).show()
+        fun playVideo() {
+            Toast.makeText(mContext, "it's playing video", Toast.LENGTH_SHORT).show()
         }
 
         @JavascriptInterface
-        fun isPlaying(isIt: Boolean) {
-            if(isIt) {
-                Toast.makeText(mContext, "The video is playing", Toast.LENGTH_SHORT).show()
-            }else{
-                Toast.makeText(mContext, "The video is not playing", Toast.LENGTH_SHORT).show()
-            }
-        }
-
-        @JavascriptInterface
-        fun performClick(string: String) {
-            Toast.makeText(mContext, string, Toast.LENGTH_SHORT).show()
+        fun pauseVideo() {
+            Toast.makeText(mContext, "it's not playing video", Toast.LENGTH_SHORT).show()
         }
     }
 }
