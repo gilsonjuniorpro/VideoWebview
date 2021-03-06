bfad7a2f7e35f54fee5c4e8fd9d55dbee70d604c
gilsonjuniorpro <gilsonjuniorpro@gmail.com>
2021-03-06 15:50:35 -0500

    added vidId to log actions

---
+++ app/src/main/java/videowebview/ca/ActionLog.kt
@@ -0,0 +1,7 @@
+package videowebview.ca
+
+class ActionLog(
+    val vidId: String,
+    val action: String,
+    val time: String
+)
--- app/src/main/java/videowebview/ca/WebAppInterface.kt
+++ app/src/main/java/videowebview/ca/WebAppInterface.kt
@@ -5,18 +5,36 @@ import android.webkit.JavascriptInterface
 import android.widget.Toast
 
 class WebAppInterface(private val mContext: Context) {
+    var listLActionLog: MutableList<ActionLog> = mutableListOf()
+
     @JavascriptInterface
-    fun playVideo() {
-        Toast.makeText(mContext, "it's playing video", Toast.LENGTH_SHORT).show()
+    fun playVideo(vidId: String, time: String) {
+        addLog(vidId, "play", time)
+        Toast.makeText(mContext, "it's playing video $time", Toast.LENGTH_SHORT).show()
     }
 
     @JavascriptInterface
-    fun pauseVideo() {
-        Toast.makeText(mContext, "it's not playing video", Toast.LENGTH_SHORT).show()
+    fun pauseVideo(vidId: String, time: String) {
+        addLog(vidId, "pause", time)
+        Toast.makeText(mContext, "it's not playing video $time", Toast.LENGTH_SHORT).show()
     }
 
     @JavascriptInterface
     fun setFullscreen() {
         Toast.makeText(mContext, "fullscreen mode", Toast.LENGTH_SHORT).show()
+    }
+
+    fun addLog(vidId: String, action: String, time: String){
+        listLActionLog.add(
+            ActionLog(
+                vidId,
+                action,
+                time
+            )
+        )
+    }
+
+    fun getLog(): MutableList<ActionLog> {
+        return listLActionLog
     }
 }
